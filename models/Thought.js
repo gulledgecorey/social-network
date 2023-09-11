dd78b2521ef8d3532cca0936d98107d81a2d8414
Corey Gulledge <coreygulledge35@gmail.com>
2023-09-11 19:28:22 -0400

    added code for  routes, models and controllers for final code

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction")
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    reactions: [
+      reactionSchema
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+   friends: [
+    {
+      type:Schema.Types.ObjectId,
+      ref: 'User'
+    }
+   ]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
